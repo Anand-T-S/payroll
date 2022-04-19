54f59f7cb8eccebd5105f6d4bbfe5ac08cf35914
Anand-T-S <100193636+Anand-T-S@users.noreply.github.com>
2022-04-19 23:24:26 +0530

    ....!!!

--- employee/urls.py
+++ employee/urls.py
@@ -3,4 +3,5 @@ from employee import views
 
 urlpatterns=[
     path("registration",views.EmployeeCreateView.as_view(),name="empregistration"),
+    path("list",views.ListAllEmployeesView.as_view(),name="employee_list")
 ]
