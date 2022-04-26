3e237426283a8d8798b39f73f82e52f10de5eac0
Anand-T-S <100193636+Anand-T-S@users.noreply.github.com>
2022-04-26 13:51:53 +0530

    forms stying

--- employee/forms.py
+++ employee/forms.py
@@ -4,12 +4,22 @@ from employee.models import Employees
 class EmployeeRegistrationForm(forms.ModelForm):
     class Meta:
         model=Employees
-        fields=[
-            "employee_name",
-            "email",
-            "user_name",
-            "password",
-            "salary",
-            "designation",
-            "dob"
-        ]
+        fields='__all__'
+        widgets={
+            'employee_name':forms.TextInput(attrs={'class':'form-control'}),
+            "email":forms.EmailInput(attrs={"class":"form-control"}),
+            "user_name":forms.TextInput(attrs={"class":"form-control"}),
+            "password":forms.PasswordInput(attrs={"class":"form-control"}),
+            "salary":forms.NumberInput(attrs={"class":"form-control"}),
+            "designation":forms.TextInput(attrs={"class":"form-control"}),
+            "dob":forms.DateInput(attrs={"class":"form-control"}),
+        }
+        # fields=[
+        #     "employee_name",
+        #     "email",
+        #     "user_name",
+        #     "password",
+        #     "salary",
+        #     "designation",
+        #     "dob"
+        # ]
