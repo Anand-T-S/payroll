0b9baa8c026f2efcae25a9745de0fc040d3e4c88
Anand-T-S <100193636+Anand-T-S@users.noreply.github.com>
2022-05-06 00:03:45 +0530

    db changed

--- employee/models.py
+++ employee/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Employees(models.Model):
     employee_name=models.CharField(max_length=80)
     email=models.EmailField(max_length=80,unique=True)
-    user_name=models.CharField(max_length=120,unique=True)
+    user_name=models.ForeignKey(User,on_delete=models.CASCADE)
     password=models.CharField(max_length=100)
     salary=models.CharField(max_length=9)
     designation=models.CharField(max_length=50)
